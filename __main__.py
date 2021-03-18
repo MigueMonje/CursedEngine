6a5b605616202ac812b0dbb06afc6b5703e85d9c
Miguel Monje <mitomomu@gmail.com>
2021-03-18 09:52:09 -0400

    Cursor añadido al ejemplo

--- __main__.py
+++ __main__.py
@@ -1,9 +1,11 @@
-from cursedengine.engine import Game
+from cursedengine.engine import Game, Cursor
+from cursedengine.math import Vector2
 import things
 from curses import wrapper
 def main(stdscr):
     game = Game(
         things.Icon(),
+        Cursor(Vector2(0,0)),
         window = stdscr
     )
     game.start()
